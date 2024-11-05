8baa2185a8b643bcf24191fbc854833f4ebd931c
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-05 13:50:07 -0300

    textos de sustentabilidade

--- src/pages/Sustentabilidade/mock/mockData.js
+++ src/pages/Sustentabilidade/mock/mockData.js
@@ -1,32 +1,27 @@
 export const cards = [
   {
     id: "card-1",
-    content:
-      "Texto1",
-    title: "Titulo1",
+    content: "Economizar combustível é uma ação fundamental para a preservação do meio ambiente. Ao optar por veículos mais eficientes ou por meios de transporte alternativos, você reduz a emissão de gases poluentes e contribui para a saúde do nosso planeta. Cada pequena mudança faz uma grande diferença na luta contra as mudanças climáticas.",
+    title: "Combustível Consciente",
   },
   {
     id: "card-2",
-    content:
-      "Texto2",
-    title: "Titulo2",
+    content: "A energia renovável é essencial para um futuro sustentável. Fontes como solar, eólica e hidrelétrica oferecem alternativas limpas que ajudam a reduzir a dependência de combustíveis fósseis. Ao investir em energia renovável, você não apenas diminui sua pegada de carbono, mas também apoia um mundo mais limpo e saudável para as próximas gerações.",
+    title: "Energia Sustentável",
   },
   {
     id: "card-3",
-    content:
-      "Texto3",
-    title: "Titulo3",
+    content: "Cada gota de água é preciosa e deve ser valorizada. O desperdício de água não apenas afeta o abastecimento, mas também gera impactos ambientais significativos. Implementar medidas simples, como consertar vazamentos e usar água de forma consciente, pode ajudar a preservar esse recurso vital, garantindo que ele esteja disponível para todos.",
+    title: "Água é Vida",
   },
   {
     id: "card-4",
-    content:
-      "Texto4",
-    title: "Titulo4",
+    content: "A mobilidade sustentável é uma parte crucial para reduzir a poluição urbana. Carros elétricos, bicicletas e transporte público são alternativas que diminuem o tráfego e as emissões. Ao optar por esses meios de transporte, você contribui para um ar mais limpo e para um ambiente mais saudável, tornando suas viagens mais ecológicas.",
+    title: "Mobilidade Verde",
   },
   {
     id: "card-5",
-    content:
-      "Texto5",
-    title: "Titulo5",
+    content: "Economizar energia em casa é uma maneira eficaz de reduzir custos e o impacto ambiental. Pequenas mudanças no dia a dia, como desligar luzes, usar eletrodomésticos eficientes e aproveitar a luz natural, podem resultar em grandes economias. Adote hábitos sustentáveis e faça a diferença para o planeta, enquanto economiza no final do mês.",
+    title: "Eficiente e Sustentável",
   },
 ];
