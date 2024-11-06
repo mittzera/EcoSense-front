0a648773066a12f3e838a17069d969ae78e3b476
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-06 15:05:17 -0300

    youtubeplayercomponent test

--- src/pages/Energia/Energia.jsx
+++ src/pages/Energia/Energia.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { VideoBanner, Card } from './components';
-import { treebg } from '../../assets';
-import { cards } from './mock/mockData';
-import ReactPlayer from 'react-player';
+  import React from 'react';
+  import { VideoBanner, Card,YoutubePlayerComponent } from './components';
+  import { treebg } from '../../assets';
+  import { cards } from './mock/mockData';
+  import ReactPlayer from 'react-player';
 
-const Energia = () => (
-  <div className="relative">
+  const Energia = () => (
+    <div className="relative">
     <div className="fixed inset-0 z-0">
       <img
         src={treebg}
-        className="absolute inset-0 w-full h-full object-cover"
+        className=" w-full h-full object-cover"
         alt="Background"
       />
     </div>
     <VideoBanner />
     
-    <div className="flex justify-center mt-8 mb-10 p-5 z-10 bg-black rounded-lg">
-      <ReactPlayer className="z-[99999999] rounded-lg overflow-hidden" url='https://www.youtube.com/watch?v=_6tTI1-VsW8' />
-    </div>
+    <YoutubePlayerComponent />
 
     <Card cards={cards} />
 
-
   </div>
-);
+  
+  );
 
-export default Energia;
+  export default Energia;
--- src/pages/Energia/components/index.js
+++ src/pages/Energia/components/index.js
@@ -1,7 +1,9 @@
 import Card from './Card';
 import VideoBanner from "./VideoBanner";
+import YoutubePlayerComponent from './YoutubePlayerComponent';
 
 export {
   Card,
   VideoBanner,
+  YoutubePlayerComponent,
 };
