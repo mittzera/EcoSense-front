8bfab2d049b28b8b2c8b3d122b8c2305a94b6a4d
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-06 15:12:06 -0300

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    https: true,
+    port: 5173, // ou outra porta de sua escolha
+  },
 })
