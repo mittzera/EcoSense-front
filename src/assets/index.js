1a9285853fe5e6f27dc4004a9f93330cf56517b7
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-08 03:18:23 -0300

    chore: assets data reorganization

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import arrowUp from "./arrow-up.svg";
 import bannerEnergia from './bannerEnergia.png';
 import bannerAgua from './bannerAgua.png';
 import bannerCombustivel from './bannerCombustivel.png';
-import sustbg from './sustentabilidadebg.png'
+import bannerSustentabilidaade from './sustentabilidadebg.png'
 import sustentabg from './sustentabg.png'
 import treebg from './treebg.png'
 import bannerEcosense from './bannerEcosense.mp4'
@@ -16,7 +16,7 @@ export {
   sustentabg,
   bannerEcosense,
   treebg,
-  sustbg,
+  bannerSustentabilidaade,
   bannerEnergia,
   bannerCombustivel,
   bannerAgua,
