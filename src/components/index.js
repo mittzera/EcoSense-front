2013402d883a9202556083eb3f2ca5b764552bfb
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-11-08 03:18:50 -0300

    chore: components import/export updated to accomodate generic components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Card from "./Card";
+import VideoBanner from "./VideoBanner"
 
 export {
+  VideoBanner,
+  Card,
   Navbar,
   Footer,
 };
